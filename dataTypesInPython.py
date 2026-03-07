dfc2b3e204ced5ddafa058027baae8078d07def5
aishwarya.p <aishwarya.p5980@gmail.com>
2026-03-07 18:13:04 +0530

    learnt datatypes in python

---
+++ dataTypesInPython.py
@@ -0,0 +1,33 @@
+# datatypes in python
+
+# No value is assigned to a variable, it is called None. It is a special constant in Python that represents the absence of a value or a null value. It is often used to indicate that a variable has no value or that a function does not return anything.
+
+# primitive datatypes in python / Single value datatypes
+# int - integer
+# float - decimal number
+# str - string
+# bool - boolean (True or False)
+
+# non-primitive datatypes in python / collection datatypes
+# list - ordered collection of items
+# tuple - ordered collection of items that cannot be changed
+# dict - unordered collection of key-value pairs
+# set - unordered collection of unique items
+# integer is a whole number without a decimal point. It can be positive, negative, or zero.
+
+a = 10
+b = 3.14
+c = "Hello, World!"
+d = 'hjk'
+e = "1223"
+f = True
+g = False
+h = None
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'float'>
+print(type(c))  # <class 'str'>
+print(type(d))  # <class 'str'>
+print(type(e))  # <class 'str'>
+print(type(f))  # <class 'bool'>
+print(type(g))  # <class 'bool'>
+print(type(h))  # <class 'NoneType'>
