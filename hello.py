1b83225dec2b54aa5e34d5bf792c07b641d14ac8
aishwarya.p <aishwarya.p5980@gmail.com>
2026-03-07 16:25:23 +0530

    learnt print statement

--- hello.py
+++ hello.py
@@ -1,9 +1,32 @@
 # start
 # single line comment
-
-#this is multi line comment
+# this is multi line comment
 # This is my 1st python code
-
 print("Hi, This is my 1st python code")
+print("hello python")
 
 
+# functions are of the types : Built in functions, user defined functions, lThird party / library functions
+# this is built in function
+print("\n========================")
+print("     Trust Python       ")
+print("========================\n")
+print('single qotes is accepted in python')
+
+#Escape sequence
+# \" - double quote
+# \' - single quote
+# \\ - backslash
+# \n - new line
+# \t - tab
+# \r - carriage return
+# \b - backspace
+# \f - form feed
+#print("double quote "something" ") #shows error
+print("double quote \"something\" \n")
+print('double quote "something" ') #this will work without escape sequence because we are using single quote to define string
+print("""\nPath:C:\\User\\aish
+Path:D:\\Data\n""") #this will work without escape sequence because we are using triple double quote to define string
+
+#exercise
+print("Your Learning Path:\n\t- Python Basics \n\t- Data Enginerring \n\t- AI\n")
